3157eee5113c28b88e7a6cf6ed2088ada0b83a47
atri60year <13690065910@163.com>
2024-11-23 19:34:44 +0800

    Site updated: 2024-11-23 19:34:44

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/11/17/hello-world/"];function toRandomPost(){
+var posts=["2024/11/23/机房珍贵图片awa/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
