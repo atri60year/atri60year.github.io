8728be5e9e126e624ac962673c36013f236bc8d0
atri60year <13690065910@163.com>
2024-12-08 02:02:16 +0800

    Site updated: 2024-12-08 02:02:16

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/11/23/机房珍贵图片awa/","2024/11/23/周杰伦全专辑免费分享/"];function toRandomPost(){
+var posts=["2024/11/23/机房珍贵图片awa/","2024/11/23/周杰伦全专辑免费分享/","2024/12/08/视频链接/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
