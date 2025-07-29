01bd7d5302dc3ef7bc7a8332293c7d164c66ef40
atri60year <13690065910@163.com>
2025-07-29 11:05:33 +0800

    Site updated: 2025-07-29 11:05:32

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/11/23/周杰伦全专辑免费分享/"];function toRandomPost(){
+var posts=["2025/07/29/修复网易云音乐云盘周杰伦歌曲/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
